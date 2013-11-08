a10c6f8188562330abe18a43b176e3c99b0d7aea
Christian de Botton <cdebotton@gmail.com>
2013-11-07 22:39:37 -0500

    Adding staging db.

--- server/app/config/staging/database.php
+++ server/app/config/staging/database.php
@@ -1,3 +1,15 @@
 <?php
 
-return [];
+return [
+  'default' => 'mysql',
+  'mysql' => [
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'bumadlib-staging',
+    'username'  => 'ula',
+    'password'  => '76FBOudt',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+  ]
+];
